b755c6be5ac7fd835fa8662df8e512dbbfe60e42
pycharm <pycharm@163.com>
2019-07-06 21:52:01 +0800

    operator1

---
+++ day2/operator1.py
@@ -0,0 +1,19 @@
+"""
++
+-
+*
+/
+//
+**
+%
+"""
+
+num1 = 13
+num2 = 4
+print(num1 + num2)  # 13
+print(num1 - num2)  # 9
+print(num1 / num2)  # 3.25
+print(num1 * num2)  # 52
+print(num2 ** 3)    # 64
+print(num1 // num2) # 3
+print(num1 % num2)  # 1
