5c31c0865e003de7437b0389c0c65ac977daaccd
pycharm <pycharm@163.com>
2019-07-16 19:35:26 +0800

    dns monitor web server

---
+++ DNS域名轮询业务监控/dns-web-monitor.py
@@ -0,0 +1,42 @@
+import dns.resolver
+import http.client
+
+ip_list = []
+
+domain = "www.baidu.com"
+
+
+def get_iplist():
+    try:
+        A = dns.resolver.query(domain, 'A')
+    except Exception as e:
+        print("get ip list error", e)
+    else:
+        for ip in A.response.answer:
+            for x in ip.items:
+                ip_list.append(ip)
+
+
+def http_check(ip):
+    url = ip + ":80"
+    try:
+        conn = http.client.HTTPConnection(url)
+        conn.request("GET", "/", headers = {"Host": domain})
+        r = conn.getresponse()
+        content = r.read(15)
+        content = content.decode("utf-8")
+    except Exception as e:
+        print("request error", e)
+    else:
+        if content == "<!DOCTYPE html>":
+            print(ip, " OK")
+        else:
+            print(ip, " Error")
+
+
+if __name__ == "__main__":
+    get_iplist()
+    for ip in ip_list:
+        http_check(str(ip))
+    else:
+        print("everything is ok")
